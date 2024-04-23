0680abb62fc455343078437a55e98c1e39222dae
roshan-jha-23 <jha062305@gmail.com>
2024-04-23 12:10:42 +0530

    landing page restored

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+'use client'
 import Image from "next/image";
-import { SignupPage } from "./signup/page";
-
+import { LandingPage } from "@/components/LandingPage";
+import Navbar from "@/components/Navbar";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <SignupPage/>
+      <h1>Auth</h1>
+      <Navbar/>
+      <LandingPage />
     </main>
   );
 }
