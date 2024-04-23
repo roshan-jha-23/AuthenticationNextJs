f9ed615af52175b9c5e62332ff5acedd38404a3f
roshan-jha-23 <jha062305@gmail.com>
2024-04-23 11:23:56 +0530

    add profie page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,42 +1,56 @@
+"use client";
 import axios from "axios";
-import { toast } from "react-hot-toast";
 import Link from "next/link";
+import React, { useState } from "react";
+import { toast } from "react-hot-toast";
 import { useRouter } from "next/navigation";
-import React from "react";
-
 
 export default function ProfilePage() {
   const router = useRouter();
-  const [data, setData] = React.useState({});
-  const getUserDetail=async()=>{
-    const res=await axios.post('/api/users/me')
-    console.log(res.data);
-    setData(res.data)
-  }
-  const logout=async()=>{
+  const [data, setData] = useState("nothing");
+  const logout = async () => {
     try {
-        await axios.get('/api/users/logout')
-        toast.success("logout Done")
-        router.push('/api/users/login')
-        setData({})
-    } catch (error:any) {
-        console.log(error.message);
-        toast.error(error.message)
-        
+      await axios.get("/api/users/logout");
+      toast.success("Logout successful");
+      router.push("/login");
+    } catch (error: any) {
+      console.log(error.message);
+      toast.error(error.message);
     }
-  }
-  return( <div>
-    <h1>Profile Page</h1>
-    <br />
-    <h2>User Information : </h2><br/><br/>
-    
-      Name : {data?.name}<br />
-      Email : {data?.email}<br />
-      
-      <button onClick={getUserDetail}>Update User Info</button>
-      <Link href="/changepassword">
-          <a>Change Password</a>
-      </Link>
-      <br/><br/>
-  </div>);
+  };
+
+  const getUserDetails = async () => {
+    const res = await axios.get("/api/users/me");
+    console.log(res.data);
+    setData(res.data.data._id);
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>Profile</h1>
+      <hr />
+      <p>Profile page</p>
+      <h2 className="p-1 rounded bg-green-500">
+        {data === "nothing" ? (
+          "Nothing"
+        ) : (
+          <Link href={`/profile/${data}`}>{data}</Link>
+        )}
+      </h2>
+      <hr />
+      <button
+        onClick={logout}
+        className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Logout
+      </button>
+
+      <button
+        onClick={getUserDetails}
+        className="bg-green-800 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        GetUser Details
+      </button>
+    </div>
+  );
 }
